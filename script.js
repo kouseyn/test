d4ba76461d12719d922232bf7680ae7d865ff726
kouseyy <113606344+kouseyn@users.noreply.github.com>
2023-08-24 22:57:40 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const responseElement = document.getElementById('response');
 
 const recognition = new window.webkitSpeechRecognition();
 
+const apiKey = "6467347f4329ae09ce8041d32edca306"; 
 recognition.lang = 'ja-JP';
 recognition.continuous = false;
 recognition.interimResults = false;
@@ -36,3 +37,43 @@ startButton.addEventListener('click', () => {
   recognition.start();
   responseElement.textContent = 'Listening...';
 });
+recognition.onresult = async (event) => {
+    const speechResult = event.results[0][0].transcript;
+  
+    let matchingResponse = responses.find(response => {
+      return speechResult.includes(response.input) || response.input.split(' ').some(word => speechResult.includes(word));
+    });
+  
+    if (matchingResponse) {
+      responseElement.textContent = matchingResponse.output;
+    } else {
+      let wordResponses = responses.filter(response => {
+        return speechResult.includes(response.input);
+      });
+  
+      if (wordResponses.length > 0) {
+        responseElement.textContent = wordResponses[0].output;
+      } else if (speechResult.includes("天気予報おしえて")) {
+        try {
+          const weather = await fetchWeather(); // 天気情報の取得
+          responseElement.textContent = `今日の天気は${weather}です！。`; // 応答生成
+        } catch (error) {
+          responseElement.textContent = "天気情報の取得に失敗しました。";
+        }
+      } else {
+        responseElement.textContent = 'すみません、よく聞き取れませんでした。';
+      }
+    }
+  };
+  
+  async function fetchWeather() {
+    try {
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Tokyo&units=metric&appid=${apiKey}`);
+      const data = await response.json();
+      const weatherDescription = data.weather[0].description;
+      return weatherDescription;
+    } catch (error) {
+      throw error;
+    }
+  }
+
